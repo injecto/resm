68ac73097df6af5eba974dea3a6f4b0bce83c20c
injecto <injecto.wg@gmail.com>
2016-02-20 16:54:05 +0300

    slight prettifying

--- src/http_handler.erl
+++ src/http_handler.erl
@@ -5,6 +5,9 @@
 -export([handle/2]).
 -export([terminate/3]).
 
+-define(JSON_CONTENT_TYPE_HEADER, {<<"content-type">>, <<"application/json; charset=utf-8">>}).
+-define(PLAIN_CONTENT_TYPE_HEADER, {<<"content-type">>, <<"text/plain; charset=utf-8">>}).
+
 init(_, Req, alloc) ->
   {User, Req2} = cowboy_req:binding(user, Req),
   {ok, Req2, {alloc, User}};
@@ -23,9 +26,9 @@ init(_, Req, Opts) ->
 handle(Req, {alloc, User} = State) ->
   case req_handler:allocate(User) of
     no_res ->
-      {ok, Req2} = cowboy_req:reply(503, [], <<"Out of resources.">>, Req);
+      {ok, Req2} = cowboy_req:reply(503, [?PLAIN_CONTENT_TYPE_HEADER], <<"Out of resources.">>, Req);
     Res ->
-      {ok, Req2} = cowboy_req:reply(201, [], integer_to_binary(Res), Req)
+      {ok, Req2} = cowboy_req:reply(201, [?PLAIN_CONTENT_TYPE_HEADER], integer_to_binary(Res), Req)
   end,
   {ok, Req2, State};
 
@@ -34,18 +37,18 @@ handle(Req, {dealloc, Res} = State) ->
     ok ->
       {ok, Req2} = cowboy_req:reply(204, Req);
     no_alloc ->
-      {ok, Req2} = cowboy_req:reply(404, [], <<"Not allocated.">>, Req)
+      {ok, Req2} = cowboy_req:reply(404, [?PLAIN_CONTENT_TYPE_HEADER], <<"Not allocated.">>, Req)
   end,
   {ok, Req2, State};
 
 handle(Req, list = State) ->
   Pool = req_handler:list(),
-  {ok, Req2} = cowboy_req:reply(200, [{<<"content-type">>, <<"application/json">>}], jsx:encode(Pool), Req),
+  {ok, Req2} = cowboy_req:reply(200, [?JSON_CONTENT_TYPE_HEADER], jsx:encode(Pool), Req),
   {ok, Req2, State};
 
 handle(Req, {user_list, User} = State) ->
   Rs = req_handler:list(User),
-  {ok, Req2} = cowboy_req:reply(200, [], jsx:encode(Rs), Req),
+  {ok, Req2} = cowboy_req:reply(200, [?JSON_CONTENT_TYPE_HEADER], jsx:encode(Rs), Req),
   {ok, Req2, State};
 
 handle(Req, reset = State) ->
@@ -55,7 +58,7 @@ handle(Req, reset = State) ->
 
 handle(Req, bad_req = State) ->
   ok = req_handler:reset(),
-  {ok, Req2} = cowboy_req:reply(400, [], <<"Bad request.">>, Req),
+  {ok, Req2} = cowboy_req:reply(400, [?PLAIN_CONTENT_TYPE_HEADER], <<"Bad request.">>, Req),
   {ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
